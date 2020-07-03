598149ab690b8d7d7c92dcb69a7ceb537c7149d4
antapiav <lenin.tapia@bbva.com>
2020-07-03 01:10:17 -0500

    4

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const app = express();
 app.use(express.static('./dist/example'));
 
 app.get('/*', function (req, res) {
-  //res.sendFile('index.html', { root: 'dist/example' });
+  response.json({ info: 'Bienvenido api practitioner' });//res.sendFile(path.join(__dirname, 'angular-build', 'index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
 
-console.log(`Running on port ${process.env.PORT || 8080}`)
+console.log(`Running on port ${process.env.PORT || 8080}`);
