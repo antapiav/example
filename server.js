6cc24edc5b0a6a64430d3fee2ff084cd62b5dfdd
antapiav <lenin.tapia@bbva.com>
2020-07-04 21:50:46 -0500

    deploy heroku

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
-app.use(express.static('./dist/example'));
+app.use(express.static(__dirname + '/dist/example'));
 
 app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/example/index.html'));
   response.json({ info: 'Bienvenido api practitioner' });//res.sendFile(path.join(__dirname, 'angular-build', 'index.html'));
 });
 
